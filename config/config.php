6d140b36b76f4b4cc4f29e40c5d7a881372d45cc
librevlad <librevlad@gmail.com>
2019-04-15 16:14:51 +0300

    Add laravel-style comment to config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,21 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hide Commands
+    |--------------------------------------------------------------------------
+    |
+    | This option allows to hide certain commands from the summary output.
+    | They will still be available in your application. Wildcards are supported
+    |
+    | Examples: "make:*", "list"
+    |
+    */
+
     'hide' => [
-        'make:*',
-        'list'
+        'list',
     ],
+
 ];
